930e02a9ae5ee6aa729219c91cac562a41c482cd
大石头 <nnhy@vip.qq.com>
2023-08-15 14:51:26 +0800

    参数保存到星尘配置中心

---
+++ A4Flower/FlowerSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace A4Flower;
+
+internal class FlowerSetting
+{
+    [Description("定时周期。默认3600秒")]
+    public Int32 Period { get; set; } = 3600;
+
+    [Description("蜂鸣器时间。默认200ms")]
+    public Int32 BuzzerTime { get; set; } = 200;
+
+    [Description("USB电源时间。默认3000ms")]
+    public Int32 UsbTime { get; set; } = 3000;
+}
--- A4Flower/Worker.cs
+++ A4Flower/Worker.cs
@@ -1,4 +1,5 @@
 ﻿using NewLife;
+using NewLife.Configuration;
 using NewLife.Log;
 using NewLife.Model;
 using NewLife.Threading;
@@ -12,18 +13,28 @@ namespace A4Flower;
 public class Worker : IHostedService
 {
     private readonly A4 _a4;
+    private FlowerSetting _setting = new();
     private readonly ITracer _tracer;
     private TimerX _timer;
 
-    public Worker(A4 a4, ITracer tracer)
+    public Worker(A4 a4, IConfigProvider configProvider, ITracer tracer)
     {
         _a4 = a4;
         _tracer = tracer;
+
+        // 绑定参数到配置中心，支持热更新
+        if (configProvider.Keys.Count < 3)
+            configProvider.Save(_setting);
+        else
+            configProvider.Bind(_setting, true, null);
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _timer = new TimerX(DoWork, null, 0, 3600_000);
+        var p = _setting.Period;
+        if (p <= 0) p = 3600;
+
+        _timer = new TimerX(DoWork, null, 0, p * 1000);
 
         return Task.CompletedTask;
     }
@@ -37,18 +48,22 @@ public class Worker : IHostedService
 
     void DoWork(Object state)
     {
+        XTrace.WriteLine("打开开关");
+
         using var span = _tracer?.NewSpan("DoWork");
         try
         {
             var buzzer = _a4.Buzzer;
             var usb = _a4.UsbPower;
 
+            var t = _setting.BuzzerTime > 0 ? _setting.BuzzerTime : 200;
             buzzer.Write(true);
-            Thread.Sleep(200);
+            Thread.Sleep(t);
             buzzer.Write(false);
 
+            t = _setting.UsbTime > 0 ? _setting.UsbTime : 3000;
             usb.Write(true);
-            Thread.Sleep(3000);
+            Thread.Sleep(t);
             usb.Write(false);
         }
         catch (Exception ex)
@@ -56,5 +71,7 @@ public class Worker : IHostedService
             span?.SetError(ex, null);
             XTrace.WriteException(ex);
         }
+
+        XTrace.WriteLine("关闭电源");
     }
 }
